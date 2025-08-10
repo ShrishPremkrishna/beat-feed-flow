071952223458e6ec51ba5051ebf9bd421e4e37df
ShrishPremkrishna <shrish.premkrishna@gmail.com>
2025-08-09 22:59:46 -0700

    custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,20 +4,23 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/thats-beatify/' : '/',
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const base = process.env.VITE_BASE_PATH || (mode === 'production' ? '/thats-beatify/' : '/');
+  return ({
+    base,
+    server: {
+      host: "::",
+      port: 8080,
     },
-  },
-}));
+    plugins: [
+      react(),
+      mode === 'development' &&
+      componentTagger(),
+    ].filter(Boolean),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  });
+});
