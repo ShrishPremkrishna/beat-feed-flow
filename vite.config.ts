811f724091c0648b375187ad8c3b9f4774fc02a9
ShrishPremkrishna <shrish.premkrishna@gmail.com>
2025-08-17 12:35:59 -0700

    wtf

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+  base: mode === 'production' ? '/thats-beatify/' : '/',
+>>>>>>> parent of 0719522 (custom domain)
 =======
   base: mode === 'production' ? '/thats-beatify/' : '/',
 >>>>>>> parent of 0719522 (custom domain)
